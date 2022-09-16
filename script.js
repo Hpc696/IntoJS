66a669a53ca05bbcdfad0132dd2cc9aa8bdb2dee
Matteo_G <honrop.giochi@gmail.com>
2022-09-16 11:36:38 +0200

    prima parte pausa yeee

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-window.console.log("Ho fatto il c+++o di caffè"); //oggetto window...che con log stampa il mex
-document.write("Ho fatto il caffè"); //
+//I commenti in master NOOOO!!!
+window.console.log("Ho fatto il c+++o di caffè"); //oggetto window...che con log stampa su console il mex
+document.write("Ho fatto il caffè"); //document "inteso come index" che con write fa stampare il mex sulla pagina
+var Tazzina= 0; //vecchio tipo--non usare--dichiarato variabile con il nome 'Tazzina' con valore = 0 (meglio sempre inizializzare la variabile con un qualsiasi valore)
+let NewTazzina=0; //stesso funzionamento di var ma da usare questo modo o il seguente:
+const caffè=3; //deve essere per forza inizializzata!
+/*altro modo di commentare*/
+let lattuga=0;
+let cipolla=0.969;
+let banane='Gialle';
+let zucchero=true;
+if (cipolla>2) { //comando if/else
+    zucchero=false;
+} else {
+    cipolla++;
+} document.write(cipolla); //stampa cipolla dopo averla incrementata di 1 con if/else
+/*pausa*/
